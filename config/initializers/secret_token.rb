5f38aee2c2a0e518ea35bb8bc0cbdd3edac36ffd
vagrant <vagrant@kellogg-dev-box>
2014-04-15 13:47:08 +0000

    Generate the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hufflepuff::Application.config.secret_key_base = '7538ce28f0b2b5074892a7b3a695483b25783df127a292896bff5aba3f83ce69770381f7172b32466189be04736bb9d1aca969a443ed687f8c87a3fc90030381'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hufflepuff::Application.config.session_store :cookie_store, key: '_hufflepuff_session'
